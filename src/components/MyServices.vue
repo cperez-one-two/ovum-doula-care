774bd99c21c9c293e94538cb575be095958c470b
cperez-one-two <christopher12perez6@gmail.com>
2023-06-26 23:03:43 -0600

    wip updating services

--- src/components/MyServices.vue
+++ src/components/MyServices.vue
@@ -1,39 +1,46 @@
 <template>
 <section id="services" class="px-5 pb-10 text-lg grid grid-cols-1 pt-11 bg-gray-50">
   <h2 class="mt-12 mb-5 font-mono text-5xl text-center md:text-6xl xl:text-7xl">Services</h2>
-  <div class="max-w-2xl mx-auto content-wrap md:max-w-6xl lg:flex">
+  <div class="max-w-2xl mx-auto content-wrap md:max-w-6xl lg:flex lg:flex-row">
     <div class="flex-1 w-full mx-auto mb-2 border shadow-lg rounded-md lg:mr-1 lg:flex-col">
-      <h3 class="py-3 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Birth Package</h3>
+      <h3 class="py-3 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Birth Packages</h3>
       <div class="relative w-full h-60">
         <g-image src="~/assets/birth-bw-3.jpg" class="absolute object-cover object-bottom w-full h-full"></g-image>
       </div>
       <div class="p-4">
-        <p>During your pregnancy it will be my top priority to provide the information, comfort, and resources you may need. The birth package includes:</p>
-        <div class="max-w-2xl mx-auto includes mx-11/12 divide-solid divide-y">
-          <div class="p-2 ">
+        <p>During your pregnancy it will be my top priority to provide the information, comfort, and resources you may need. All birth packages include unlimited phone and text support, prenatal visit(s), in person birth support, and a postpartum visit. I also offer photographs with all packages.</p>
+        <p>The areas covered in the prenatal visit(s) include the stages of labor, advocacy and intervention, postpartum healing, comfort measures, feeding, and pelvic dynamics among other topics. While supporting your birth in person, I will be focusing on encouraging your birth preferences, your comfort, advocating in each birth setting, birth positions, and explaining what may to expect. I will also be focused on supporting your partner so that they can be more present during the birth. After the baby is born, I will stay to help with the first latch or the first feed. The postpartum visit is a space to discuss the birth, ask any questions about infant care or feeding, and provide further resources. 
+</p>
+        <div class="max-w-6xl justify-center mx-auto includes mx-11/12 divide-solid divide-y lg:flex">
+
+          <div class="p-2 max-w-xs">
+            <h3 class="py-1 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Base Package</h3>
             <div class="my-2">
               <svg class="w-10 h-10 mx-auto fill-current hover:text-yellow text-olive-dark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                 <path d="M17 3h-1v2h-3V3H7v2H4V3H3c-1.101 0-2 .9-2 2v12c0 1.1.899 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 14H3V9h14v8zM6.5 1h-2v3.5h2V1zm9 0h-2v3.5h2V1z"/>
               </svg>
             </div>
             <!-- <span class="block my-2 text-3xl text-center text-olive-dark"><font-awesome :icon="['fas', 'hand-holding-medical']" size="1x"/></span> -->
-            <p>Two prenatal appointments covering a wide range of topics including the stages of labor, comfort preferences, and positioning. This is also a space to discuss postpartum, breastfeeding, and interventions. As a certified Yoga instructor and Reiki practitioner, I can guide you through movements during your pregnancy to increase comfort.</p>
+            <p>The base package includes unlimited text and phone support, <em><b>one</b></em> prenatal visit, on-call birth support, and a postpartum visit. </p>
           </div>
-          <div class="p-2">
+          <div class="p-2 max-w-xs">
+            <h3 class="py-1 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Standard Package</h3>
             <!-- <span class="block my-2 text-3xl text-center text-olive-dark"><font-awesome :icon="['fas', 'hands']" size="1x"/></span> -->
             <div class="my-2">
               <svg version="1.1" class="w-10 h-10 mx-auto stroke-current hover:text-yellow text-olive-dark" fill="none" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 38 38" enable-background="new 0 0 38 38" xml:space="preserve">
                 <path stroke-width="2" stroke-miterlimit="10" d="M23,8c0,2.209-1.791,4-4,4s-4-1.791-4-4 c0-0.732,0-2.268,0-3c0-2.209,1.791-4,4-4s4,1.791,4,4C23,5.732,23,7.268,23,8z M28.496,33.194l-7.113-3.401L9.438,33.194 M15,20 l-1.002,6.624c0,0-3.892,1.753-5.482,2.581C6.925,30.036,5,31.096,5,33.688c0,2.063,1.483,3.396,3.483,3.396S15,35.794,19,34.714 c4-1.081,7.806-2.328,7.806-2.328 M19,34.714C23,35.794,27.517,37,29.517,37S33,35.667,33,33.604c0-2.592-1.925-3.666-3.516-4.496 c-1.591-0.828-5.482-2.608-5.482-2.608L23,20 M0.503,31.318c0,0,4.435-3.147,6.435-6.366S10.856,17.396,12,16s5-2.552,5-2.552 v-1.986 M21,11.462v1.986c0,0,3.855,1.156,5,2.552c1.144,1.396,3.062,5.733,5.062,8.952s6.435,6.366,6.435,6.366"/>
               </svg>
             </div>
-            <p>In-person support throughout your labor and birth. During labor, I will assist you and your partner physically and emotionally and will aid in decision making processes if questions arise. I will also assist with position changes and techniques to bring your baby down in the smoothest way possible. It is my goal to help reach informed consent in all choices and to be there every step of the way.</p>
+            <p>The standard package includes unlimited text and phone support, <em><b>two</b></em> prenatal visits, on-call birth support, and a postpartum visit. This package also includes an herbal sitz bath, herbal steam, and perineum/nipple/belly salve for postpartum healing. Additionally, this package includes an eBook on birth physiology, intervention and advocacy, feeding basics, and perinatal mental health written by Cali Machen CD and Maggie Saltiel LSW.</p>
           </div>
-          <div class="p-2">
+          <div class="p-2 max-w-xs">
             <!-- <span class="block my-2 text-3xl text-center text-olive-dark"><font-awesome :icon="['fas', 'people-arrows']" size="1x"/></span> -->
+          <h3 class="py-1 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Advanced Package</h3>
             <div class="my-2">
               <svg class="mx-auto stroke-current hover:text-yellow w-9 h-9 text-olive-dark" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"/></svg>
             </div>
-            <p>Visit after the baby is born to provide breastfeeding support and information around postpartum recovery.</p>
+            <p>The advanced package includes unlimited text and phone support, <b><em>three</em></b> prenatal visits, on-call support, and <b><em>two</em> </b> postpartum visits. This package also includes an herbal sitz bath, herbal steam, and perineum/nipple/belly salve for postpartum healing. Additionally, this package includes an eBook on birth physiology, intervention and advocacy, feeding basics, and perinatal mental health written by Cali Machen CD, and Maggie Saltiel LSW.	
+</p>
           </div>
         </div>
       </div>
@@ -42,39 +49,39 @@
       <!--   <p><span class="font-bold">$1000</span> per birth</p> -->
       <!-- </div> -->
     </div>
-    <div class="justify-between flex-1 w-full mx-auto mb-2 border shadow-lg rounded-md lg:ml-1 lg:flex-col">
-      <h3 class="py-3 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Postpartum Services</h3>
-      <div class="relative w-full h-60">
-        <g-image src="~/assets/birth-bw-1.jpg" class="absolute object-cover object-center w-full h-full"></g-image>
-      </div>
-      <div class="p-4">
-        <p>As your postpartum doula, I am trained to support your family and infant during the postpartum period. Postpartum services include but are not limited to:</p>
-        <div class="max-w-2xl mx-auto includes mx-11/12 divide-solid divide-y">
-          <div class="p-2 lg:py-12">
-            <ul class="list-disc list-inside">
-              <li>Infant care (bathing, feeding, diapering, and soothing)</li>
-              <li>Sibling care</li>
-              <li>Feeding support</li>
-              <li>Referrals and resources</li>
-              <li>Postpartum physical/emotional support</li>
-              <li>Errands</li>
-              <li>Light housekeeping</li>
-              <li>Meal preparation</li>
-            </ul>
-          </div>
-          <div class="p-2 lg:py-8">
-            <svg class="w-10 h-10 mx-auto fill-current text-olive-dark hover:text-yellow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-              <path d="M17.19 4.156c-1.672-1.535-4.383-1.535-6.055 0L10 5.197 8.864 4.156c-1.672-1.535-4.382-1.535-6.054 0-1.881 1.726-1.881 4.519 0 6.245L10 17l7.19-6.599c1.88-1.726 1.88-4.52 0-6.245zm-1.066 5.219L10 15.09 3.875 9.375c-.617-.567-.856-1.307-.856-2.094s.138-1.433.756-1.999c.545-.501 1.278-.777 2.063-.777.784 0 1.517.476 2.062.978L10 7.308l2.099-1.826c.546-.502 1.278-.978 2.063-.978s1.518.276 2.063.777c.618.566.755 1.212.755 1.999s-.238 1.528-.856 2.095z"/>
-            </svg>
-            <p class="pt-3">I am also trained to look for signs of postpartum mood disorders following the birth and equipped with the resources to help clients get the support they need. In addition to birth and postpartum support, I can provide the resources to help you deepen your understanding of your fertility, menstrual and ovulation cycle and can help you find options for non-hormonal contraception and conception methods</p>
-          </div>
-        </div>
-      </div>
-      <!-- <div class="text-center bg-yellow-50 rounded-b-md"> -->
-      <!--   <h4 class="text-2xl font-bold ">Pricing</h4> -->
-      <!--   <p><span class="font-bold">$20/hr</span> daytime, <span class="font-bold">$25/hr</span> overnight</p> -->
-      <!-- </div> -->
-    </div>
+    <!-- <div class="justify-between flex-1 w-full mx-auto mb-2 border shadow-lg rounded-md lg:ml-1 lg:flex-col"> -->
+    <!--   <h3 class="py-3 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Postpartum Services</h3> -->
+    <!--   <div class="relative w-full h-60"> -->
+    <!--     <g-image src="~/assets/birth-bw-1.jpg" class="absolute object-cover object-center w-full h-full"></g-image> -->
+    <!--   </div> -->
+    <!--   <div class="p-4"> -->
+    <!--     <p>As your postpartum doula, I am trained to support your family and infant during the postpartum period. Postpartum services include but are not limited to:</p> -->
+    <!--     <div class="max-w-2xl mx-auto includes mx-11/12 divide-solid divide-y"> -->
+    <!--       <div class="p-2 lg:py-12"> -->
+    <!--         <ul class="list-disc list-inside"> -->
+    <!--           <li>Infant care (bathing, feeding, diapering, and soothing)</li> -->
+    <!--           <li>Sibling care</li> -->
+    <!--           <li>Feeding support</li> -->
+    <!--           <li>Referrals and resources</li> -->
+    <!--           <li>Postpartum physical/emotional support</li> -->
+    <!--           <li>Errands</li> -->
+    <!--           <li>Light housekeeping</li> -->
+    <!--           <li>Meal preparation</li> -->
+    <!--         </ul> -->
+    <!--       </div> -->
+    <!--       <div class="p-2 lg:py-8"> -->
+    <!--         <svg class="w-10 h-10 mx-auto fill-current text-olive-dark hover:text-yellow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"> -->
+    <!--           <path d="M17.19 4.156c-1.672-1.535-4.383-1.535-6.055 0L10 5.197 8.864 4.156c-1.672-1.535-4.382-1.535-6.054 0-1.881 1.726-1.881 4.519 0 6.245L10 17l7.19-6.599c1.88-1.726 1.88-4.52 0-6.245zm-1.066 5.219L10 15.09 3.875 9.375c-.617-.567-.856-1.307-.856-2.094s.138-1.433.756-1.999c.545-.501 1.278-.777 2.063-.777.784 0 1.517.476 2.062.978L10 7.308l2.099-1.826c.546-.502 1.278-.978 2.063-.978s1.518.276 2.063.777c.618.566.755 1.212.755 1.999s-.238 1.528-.856 2.095z"/> -->
+    <!--         </svg> -->
+    <!--         <p class="pt-3">I am also trained to look for signs of postpartum mood disorders following the birth and equipped with the resources to help clients get the support they need. In addition to birth and postpartum support, I can provide the resources to help you deepen your understanding of your fertility, menstrual and ovulation cycle and can help you find options for non-hormonal contraception and conception methods</p> -->
+    <!--       </div> -->
+    <!--     </div> -->
+    <!--   </div> -->
+    <!--   <\!-- <div class="text-center bg-yellow-50 rounded-b-md"> -\-> -->
+    <!--   <\!--   <h4 class="text-2xl font-bold ">Pricing</h4> -\-> -->
+    <!--   <\!--   <p><span class="font-bold">$20/hr</span> daytime, <span class="font-bold">$25/hr</span> overnight</p> -\-> -->
+    <!--   <\!-- </div> -\-> -->
+    <!-- </div> -->
   </div>
 </section>
 </template>
