c8787f9cc52dcbc97927dd523ee82e12ba5c4d10
cperez-one-two <christopher12perez6@gmail.com>
2023-07-28 16:41:21 -0600

    add addon services

--- src/components/MyServices.vue
+++ src/components/MyServices.vue
@@ -12,9 +12,8 @@
         <p>The areas covered in the prenatal visit(s) include the stages of labor, advocacy and intervention, postpartum healing, comfort measures, feeding, and pelvic dynamics among other topics. While supporting your birth in person, I will be focusing on encouraging your birth preferences, your comfort, advocating in each birth setting, birth positions, and explaining what may to expect. I will also be focused on supporting your partner so that they can be more present during the birth. After the baby is born, I will stay to help with the first latch or the first feed. The postpartum visit is a space to discuss the birth, ask any questions about infant care or feeding, and provide further resources. 
         </p>
       </div>
-      <div class="max-w-6xl justify-center mx-auto includes mb-2 divide-solid divide-y lg:flex">
-        
-        <div class="m-2 max-w-xs shadow-lg rounded-md mx-auto flex flex-col justify-start"> 
+      <div class="max-w-6xl justify-center p-4 mx-auto includes mb-2 divide-solid divide-y lg:flex">
+        <div class="m-2 max-w-md lg:max-w-xs shadow-lg rounded-md mx-auto flex flex-col justify-start"> 
           <h3 class="flex-none py-1 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Base Package</h3>
           <div class="flex-none">
             <div class="my-2 flex justify-center">
@@ -29,78 +28,109 @@
               >Contact for booking</a>
           </div>
         </div>
-        <div class="m-2 max-w-xs shadow-lg rounded-md mx-auto flex flex-col justify-start"> 
+        <div class="m-2 max-w-md lg:max-w-xs shadow-lg rounded-md mx-auto flex flex-col justify-start"> 
           <h3 class="flex-none py-1 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Standard Package</h3>
           <div class="flex-none">
             <div class="my-2 flex justify-center">
               <svg class="w-10 h-10 mx-1 fill-current hover:text-yellow text-olive-dark" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224 0a80 80 0 1 1 0 160A80 80 0 1 1 224 0zM436.8 382.8L373.5 462c-16.6 20.7-46.8 24.1-67.5 7.5c-17.6-14.1-22.7-38.1-13.5-57.7l-.8-.1c-38.9-5.6-74.3-25.1-99.7-54.8V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v48c0 .8 0 1.6 .1 2.4l101.4 50.7c23.7 11.9 33.3 40.7 21.5 64.4s-40.7 33.3-64.4 21.5L27.2 427.3c-1.1-.5-2.2-1.1-3.3-1.7c-4.9-2.8-9.2-6.4-12.6-10.6c-4.6-5.4-7.8-11.7-9.6-18.4c-3.3-12-1.9-25.2 4.8-36.6c.6-1.1 1.3-2.2 2-3.2L75.6 256.1c26.7-40.1 71.7-64.1 119.8-64.1h75.2c46.5 0 90.1 22.5 117.2 60.3l50.7 70.9c2.2 3 4 6.1 5.5 9.4c2.9 6.7 4.3 13.8 4 20.8c-.3 10.6-4.2 21-11.2 29.4zM320 332a44 44 0 1 0 -88 0 44 44 0 1 0 88 0z"/></svg>
-</svg>
-</div>
-<p class="m-3">The standard package includes unlimited text and phone support, <em><b>two</b></em> prenatal visits, on-call birth support, and a postpartum visit. This package also includes an herbal sitz bath, herbal steam, and perineum/nipple/belly salve for postpartum healing. Additionally, this package includes an eBook on birth physiology, intervention and advocacy, feeding basics, and perinatal mental health written by Cali Machen CD and Maggie Saltiel LSW.</p>
-</div>
-<div class="flex grow mt-auto">
-  <a
-    href="#contact"
-    class="block justify-center px-3 py-1 my-4 mx-auto bg-white border-2 hover:border-transparent rounded border-opacity-40 bg-opacity-20 hover:bg-yellow hover:border-opacity-40 transition ease-in duration-130 text-gray-950 hover:text-gray-50"
-    >Contact for booking</a>
-</div>
-</div>
-<div class="m-2 max-w-xs shadow-lg rounded-md mx-auto flex flex-col justify-start"> 
-  <h3 class="flex-none py-1 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Advanced Package</h3>
-  <div class="flex-none">
-    <div class="my-2 flex justify-center">
-      <svg class="w-10 h-10 mx-1 fill-current hover:text-yellow text-olive-dark" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M320 0a40 40 0 1 1 0 80 40 40 0 1 1 0-80zm44.7 164.3L375.8 253c1.6 13.2-7.7 25.1-20.8 26.8s-25.1-7.7-26.8-20.8l-4.4-35h-7.6l-4.4 35c-1.6 13.2-13.6 22.5-26.8 20.8s-22.5-13.6-20.8-26.8l11.1-88.8L255.5 181c-10.1 8.6-25.3 7.3-33.8-2.8s-7.3-25.3 2.8-33.8l27.9-23.6C271.3 104.8 295.3 96 320 96s48.7 8.8 67.6 24.7l27.9 23.6c10.1 8.6 11.4 23.7 2.8 33.8s-23.7 11.4-33.8 2.8l-19.8-16.7zM40 64c22.1 0 40 17.9 40 40v40 80 40.2c0 17 6.7 33.3 18.7 45.3l51.1 51.1c8.3 8.3 21.3 9.6 31 3.1c12.9-8.6 14.7-26.9 3.7-37.8l-15.2-15.2-32-32c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l32 32 15.2 15.2 0 0 25.3 25.3c21 21 32.8 49.5 32.8 79.2V464c0 26.5-21.5 48-48 48H173.3c-17 0-33.3-6.7-45.3-18.7L28.1 393.4C10.1 375.4 0 351 0 325.5V224 160 104C0 81.9 17.9 64 40 64zm560 0c22.1 0 40 17.9 40 40v56 64V325.5c0 25.5-10.1 49.9-28.1 67.9L512 493.3c-12 12-28.3 18.7-45.3 18.7H400c-26.5 0-48-21.5-48-48V385.1c0-29.7 11.8-58.2 32.8-79.2l25.3-25.3 0 0 15.2-15.2 32-32c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3l-32 32-15.2 15.2c-11 11-9.2 29.2 3.7 37.8c9.7 6.5 22.7 5.2 31-3.1l51.1-51.1c12-12 18.7-28.3 18.7-45.3V224 144 104c0-22.1 17.9-40 40-40z"/></svg>
+            </div>
+            <p class="m-3">The standard package includes unlimited text and phone support, <em><b>two</b></em> prenatal visits, on-call birth support, and a postpartum visit. This package also includes an herbal sitz bath, herbal steam, and perineum/nipple/belly salve for postpartum healing. Additionally, this package includes an eBook on birth physiology, intervention and advocacy, feeding basics, and perinatal mental health written by Cali Machen CD and Maggie Saltiel LSW.</p>
+          </div>
+          <div class="flex grow mt-auto">
+            <a
+              href="#contact"
+              class="block justify-center px-3 py-1 my-4 mx-auto bg-white border-2 hover:border-transparent rounded border-opacity-40 bg-opacity-20 hover:bg-yellow hover:border-opacity-40 transition ease-in duration-130 text-gray-950 hover:text-gray-50"
+              >Contact for booking</a>
+          </div>
+        </div>
+        <div class="m-2 max-w-md lg:max-w-xs shadow-lg rounded-md mx-auto flex flex-col justify-start"> 
+          <h3 class="flex-none py-1 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Advanced Package</h3>
+          <div class="flex-none">
+            <div class="my-2 flex justify-center">
+              <svg class="w-10 h-10 mx-1 fill-current hover:text-yellow text-olive-dark" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M320 0a40 40 0 1 1 0 80 40 40 0 1 1 0-80zm44.7 164.3L375.8 253c1.6 13.2-7.7 25.1-20.8 26.8s-25.1-7.7-26.8-20.8l-4.4-35h-7.6l-4.4 35c-1.6 13.2-13.6 22.5-26.8 20.8s-22.5-13.6-20.8-26.8l11.1-88.8L255.5 181c-10.1 8.6-25.3 7.3-33.8-2.8s-7.3-25.3 2.8-33.8l27.9-23.6C271.3 104.8 295.3 96 320 96s48.7 8.8 67.6 24.7l27.9 23.6c10.1 8.6 11.4 23.7 2.8 33.8s-23.7 11.4-33.8 2.8l-19.8-16.7zM40 64c22.1 0 40 17.9 40 40v40 80 40.2c0 17 6.7 33.3 18.7 45.3l51.1 51.1c8.3 8.3 21.3 9.6 31 3.1c12.9-8.6 14.7-26.9 3.7-37.8l-15.2-15.2-32-32c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l32 32 15.2 15.2 0 0 25.3 25.3c21 21 32.8 49.5 32.8 79.2V464c0 26.5-21.5 48-48 48H173.3c-17 0-33.3-6.7-45.3-18.7L28.1 393.4C10.1 375.4 0 351 0 325.5V224 160 104C0 81.9 17.9 64 40 64zm560 0c22.1 0 40 17.9 40 40v56 64V325.5c0 25.5-10.1 49.9-28.1 67.9L512 493.3c-12 12-28.3 18.7-45.3 18.7H400c-26.5 0-48-21.5-48-48V385.1c0-29.7 11.8-58.2 32.8-79.2l25.3-25.3 0 0 15.2-15.2 32-32c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3l-32 32-15.2 15.2c-11 11-9.2 29.2 3.7 37.8c9.7 6.5 22.7 5.2 31-3.1l51.1-51.1c12-12 18.7-28.3 18.7-45.3V224 144 104c0-22.1 17.9-40 40-40z"/></svg>
+            </div>
+            <p class="m-3">The advanced package includes unlimited text and phone support, <em><b>three</b></em> prenatal visits, on-call support, and <em><b>two</b></em> postpartum visits. This package also includes an herbal sitz bath, herbal steam, and perineum/nipple/belly salve for postpartum healing. Additionally, this package includes an eBook on birth physiology, intervention and advocacy, feeding basics, and perinatal mental health written by Cali Machen CD, and Maggie Saltiel LSW.</p>
+          </div>
+          <div class="flex grow mt-auto">
+            <a
+              href="#contact"
+              class="block justify-center px-3 py-1 my-4 mx-auto bg-white border-2 hover:border-transparent rounded border-opacity-40 bg-opacity-20 hover:bg-yellow hover:border-opacity-40 transition ease-in duration-130 text-gray-950 hover:text-gray-50"
+              >Contact for booking</a>
+          </div>
+        </div>
+      </div>
+      <div id="addons" class="shadow-xl mx-5 mb-10 text-lg mt-5 bg-gray-50 rounded-md">
+        <h3 class="py-3 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Build Your Birth Plan Package</h3>
+        <div class="p-2 m-6">
+          <p>This package includes a 3 hour session either in person or via zoom to discuss and create your birth and postpartum plan.</p> 
+          <p></p>
+          <p><em>*This package does not include on-call birth support</em></p>
+          <div class="flex grow mt-auto my-2 py-2">
+            <a
+              href="#contact"
+              class="block justify-center px-3 py-2 mx-auto bg-white border-2 hover:border-transparent rounded border-opacity-40 bg-opacity-20 hover:bg-yellow hover:border-opacity-40 transition ease-in duration-130 text-gray-950 hover:text-gray-50"
+              >Contact for booking</a>
+          </div>
+        </div>
+      </div>
+      <div id="addons" class="shadow-xl mx-5 mb-10 text-lg mt-5 pb-3 bg-gray-50 rounded-md">
+        <h3 class="py-3 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Add-ons</h3>
+        <div class="p-2 m-6">
+          <p>These services can be added to any birth package.</p>
+        </div>
+        <div class="mx-6 mt-6 mb-0 p-4 rounded-md">
+          <h4 class=""><b>Belly Binding</b></h4>
+          <hr>
+          <div class="lg:flex">
+            <p class="m-6">Belly binding is a traditional practice in many areas of the world. The type I use is called Bengkung Belly binding which is a traditional Malaysian practice. It is done with a long piece of fabric and can help with postpartum physical abdominal support for the first 40 days after birth</p>
+            <div class="relative lg:w-full h-40 m-4">
+              <!-- <div class="relative lg:w-full h-60 m-4"> -->
+                <!-- <g-image src="~/assets/birth-4.jpeg" class="absolute object-cover object-center w-full h-full rounded-sm"></g-image> -->
+              </div>
+            </div>
+          </div>
+          <div class="m-6 p-4 rounded-md">
+            <h4 class=""><b>Placenta Prints</b></h4>
+            <hr>
+            <div class="lg:flex">
+              <div class="relative lg:w-full h-60 m-4">
+                <g-image src="~/assets/placenta-art-3.jpg" class="absolute object-cover object-center w-full h-full rounded-sm"></g-image>
+              </div>
+              <p class="m-6">One way to honor the placenta is to make a print of it on the canvas either with blood or with ink. This print shows the unique “tree of life” pattern of each placenta and is a sweet way to recognize the organ that nourished your baby for many months without keeping the organ itself.</p>
+            </div>
+          </div>
+        </div>
+        
+      <div id="addons" class="shadow-xl mx-5 mb-10 text-lg mt-5 bg-gray-50 rounded-md">
+        <h3 class="py-3 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Preparing for birth workshop</h3>
+        <div class="p-2 m-6">
+          <p>Offered quarterly, this virtual workshop is created by Cali Machen, CD., and Maggie Saltiel, LSW., and focuses on a large range of prenatal education topics with a focus on mental health. Topics covered include:</p>
+          <br>
+          <ul>
+            <li>- Anatomy of Pregnancy</li>
+            <li>- Physiological Birth and Stages of Labor </li>
+            <li>- Intervention and advocacy </li>
+            <li>- Basics of Feeding</li>
+            <li>- Perinatal Mental Health </li>
+            <li>- Postpartum Healing and Support</li>
+          </ul>
+          <br>
+          <p>Those who participate in the workshop will receive a copy of our eBook and workbook to accompany the class.</p>
+          <br>
+          <p>Please contact to see when the next workshop is being offered! </p>
+          <div class="flex grow mt-auto my-2 py-2">
+            <a
+              href="#contact"
+              class="block justify-center px-3 py-2 mx-auto bg-white border-2 hover:border-transparent rounded border-opacity-40 bg-opacity-20 hover:bg-yellow hover:border-opacity-40 transition ease-in duration-130 text-gray-950 hover:text-gray-50"
+              >Contact for booking</a>
+          </div>
+        </div>
+      </div>
+      </div>
+
     </div>
-    <p class="m-3">The advanced package includes unlimited text and phone support, <em><b>three</b></em> prenatal visits, on-call support, and <em><b>two</b></em> postpartum visits. This package also includes an herbal sitz bath, herbal steam, and perineum/nipple/belly salve for postpartum healing. Additionally, this package includes an eBook on birth physiology, intervention and advocacy, feeding basics, and perinatal mental health written by Cali Machen CD, and Maggie Saltiel LSW.</p>
-  </div>
-  <div class="flex grow mt-auto">
-    <a
-      href="#contact"
-      class="block justify-center px-3 py-1 my-4 mx-auto bg-white border-2 hover:border-transparent rounded border-opacity-40 bg-opacity-20 hover:bg-yellow hover:border-opacity-40 transition ease-in duration-130 text-gray-950 hover:text-gray-50"
-      >Contact for booking</a>
   </div>
 </div>
-</div>
-</div>
-</div>
-<!-- <div class="text-center bg-yellow-50 rounded-b-md"> -->
-  <!--   <h4 class="text-2xl font-bold ">Pricing</h4> -->
-  <!--   <p><span class="font-bold">$1000</span> per birth</p> -->
-  <!-- </div> -->
-</div>
-<!-- <div class="justify-between flex-1 w-full mx-auto mb-2 border shadow-lg rounded-md lg:ml-1 lg:flex-col"> -->
-  <!--   <h3 class="py-3 font-mono text-3xl text-center bg-olive-dark rounded-t-md text-gray-50">Postpartum Services</h3> -->
-  <!--   <div class="relative w-full h-60"> -->
-    <!--     <g-image src="~/assets/birth-bw-1.jpg" class="absolute object-cover object-center w-full h-full"></g-image> -->
-    <!--   </div> -->
-  <!--   <div class="p-4"> -->
-    <!--     <p>As your postpartum doula, I am trained to support your family and infant during the postpartum period. Postpartum services include but are not limited to:</p> -->
-    <!--     <div class="max-w-2xl mx-auto includes mx-11/12 divide-solid divide-y"> -->
-      <!--       <div class="p-2 lg:py-12"> -->
-        <!--         <ul class="list-disc list-inside"> -->
-          <!--           <li>Infant care (bathing, feeding, diapering, and soothing)</li> -->
-          <!--           <li>Sibling care</li> -->
-          <!--           <li>Feeding support</li> -->
-          <!--           <li>Referrals and resources</li> -->
-          <!--           <li>Postpartum physical/emotional support</li> -->
-          <!--           <li>Errands</li> -->
-          <!--           <li>Light housekeeping</li> -->
-          <!--           <li>Meal preparation</li> -->
-          <!--         </ul> -->
-        <!--       </div> -->
-      <!--       <div class="p-2 lg:py-8"> -->
-        <!--         <svg class="w-10 h-10 mx-auto fill-current text-olive-dark hover:text-yellow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"> -->
-          <!--           <path d="M17.19 4.156c-1.672-1.535-4.383-1.535-6.055 0L10 5.197 8.864 4.156c-1.672-1.535-4.382-1.535-6.054 0-1.881 1.726-1.881 4.519 0 6.245L10 17l7.19-6.599c1.88-1.726 1.88-4.52 0-6.245zm-1.066 5.219L10 15.09 3.875 9.375c-.617-.567-.856-1.307-.856-2.094s.138-1.433.756-1.999c.545-.501 1.278-.777 2.063-.777.784 0 1.517.476 2.062.978L10 7.308l2.099-1.826c.546-.502 1.278-.978 2.063-.978s1.518.276 2.063.777c.618.566.755 1.212.755 1.999s-.238 1.528-.856 2.095z"/> -->
-          <!--         </svg> -->
-        <!--         <p class="pt-3">I am also trained to look for signs of postpartum mood disorders following the birth and equipped with the resources to help clients get the support they need. In addition to birth and postpartum support, I can provide the resources to help you deepen your understanding of your fertility, menstrual and ovulation cycle and can help you find options for non-hormonal contraception and conception methods</p> -->
-        <!--       </div> -->
-      <!--     </div> -->
-    <!--   </div> -->
-  <!--   <\!-- <div class="text-center bg-yellow-50 rounded-b-md"> -\-> -->
-    <!--   <\!--   <h4 class="text-2xl font-bold ">Pricing</h4> -\-> -->
-      <!--   <\!--   <p><span class="font-bold">$20/hr</span> daytime, <span class="font-bold">$25/hr</span> overnight</p> -\-> -->
-        <!--   <\!-- </div> -\-> -->
-          <!-- </div> -->
 </div>
 </section>
 </template>
